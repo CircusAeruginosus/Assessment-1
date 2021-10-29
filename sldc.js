f74432fc90285ebd73a52d4b11d72fad28810c6a
Caitlin Thompson <ctt00297@gmail.com>
2021-10-29 09:58:14 -0500

    Problem 2 Completed

--- sldc.js
+++ sldc.js
@@ -27,3 +27,15 @@
 /*
     Create a variable for each of the above steps of the Software Development Lifecycle and set the values of each variable to a definition of the step.
 */
+
+let planning = "outlining the scope of the problem and identifying potential solutions"
+
+let analysisOfRequirements = "collecting all relevant information and requirements from the customer, analyzing the feasibility of developing the requested product, and proposing a solution to the problem"
+
+let design = "using the information and requirements collected to plan out the architecture of the product and outline how each feature in the product should work "
+
+let implementation = "translating the design into code, creating data in the database, and creating interfaces"
+
+let testingAndIntegration = "testing the code against the requirements to find defects and verify whether the product works as expected; fixing defects if found"
+
+let maintain = "making necessary upgrades, changes, and adding new features once a product has been deployed"
