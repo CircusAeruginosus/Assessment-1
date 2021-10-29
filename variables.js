26ba12573cc875bc1c6ae3b074e7d31fc035c6e7
Caitlin Thompson <ctt00297@gmail.com>
2021-10-29 10:03:15 -0500

    Problems 2 & 3 Completed

--- variables.js
+++ variables.js
@@ -2,18 +2,20 @@
 /* Create a variable called 'myName' with a value that is your name as a string. */
 
 //CODE HERE 
-
 const myName = "Caitlin Thompson"
 
 //////////////////PROBLEM 2////////////////////
 /* Create a variable called 'faveNum' with a value that is your favorite number. */
 
 //CODE HERE
+const faveNum = 18
 
 //////////////////PROBLEM 3////////////////////
 /* Create a variable called 'lovesCode' and assign it a boolean value. */
 
 //CODE HERE
+let lovesCode = true
+
 
 //////////////////PROBLEM 4////////////////////
 /*
